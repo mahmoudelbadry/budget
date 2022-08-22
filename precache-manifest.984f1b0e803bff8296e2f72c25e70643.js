cacb01ea6f48a41b185f205a43a06f14e6d356a8
Mahmoud El-Badry <mahmoud.elbadry@mokalmat.com>
2022-08-22 11:53:05 +0200

    first commit

---
+++ precache-manifest.984f1b0e803bff8296e2f72c25e70643.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc558ae5033c45fa5414",
+    "url": "/static/js/main.bc558ae5.chunk.js"
+  },
+  {
+    "revision": "9a0b9cc8b05498bd792f",
+    "url": "/static/js/1.9a0b9cc8.chunk.js"
+  },
+  {
+    "revision": "7346e0eb7b3560a24a52",
+    "url": "/static/js/2.7346e0eb.chunk.js"
+  },
+  {
+    "revision": "c6625768396c6a6b7816",
+    "url": "/static/css/3.91080a46.chunk.css"
+  },
+  {
+    "revision": "c6625768396c6a6b7816",
+    "url": "/static/js/3.c6625768.chunk.js"
+  },
+  {
+    "revision": "565d502f847cdad43924",
+    "url": "/static/js/runtime~main.565d502f.js"
+  },
+  {
+    "revision": "3a105e2b7f58f5e27a500fe39f8fd164",
+    "url": "/static/js/browser-server.06b54be5.worker.js"
+  },
+  {
+    "revision": "ad9dce3bddd59033381d5755528c1809",
+    "url": "/static/media/bg.ad9dce3b.svg"
+  },
+  {
+    "revision": "1ae926741322a7cdb33553c7d00c1318",
+    "url": "/static/media/DateSelect.left.1ae92674.png"
+  },
+  {
+    "revision": "2a470dc002854e63925688e856f2a400",
+    "url": "/static/media/DateSelect.right.2a470dc0.png"
+  },
+  {
+    "revision": "0c47b21c758d10d1dfbc0f2d3ab8fbb0",
+    "url": "/index.html"
+  }
+];
